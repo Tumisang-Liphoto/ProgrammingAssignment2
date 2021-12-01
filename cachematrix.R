84137d5bcb352b0ccf3218b41568a7c5533fbe54
Tumisang-Liphoto <mrsechocha@gmail.com>
2021-12-01 18:20:23 +0200

    update  makeCacheMatrix andcacheSolve
    solution for Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+#setwd('')#remember to delete path and comment line
 
-## Write a short comment describing this function
+##function will take matrix (square), use solve(x) to create inverse of matrix
+##then store the result in cache
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(testMatrix = matrix()) { #create matrix 
+  
+  result <- NULL #create null object, solution will be saved here
+  set <- function(y){
+    testMatrix <<- y #store in cache
+    result <<- NULL
+  }
+  #create inverse of matrix, no need to verify if invertible
+  get <- function() testMatrix
+  setsolve <- function(solve) {
+    result <<- solve
+  }
+  
+  getsolve <- function() result
+  list(set=set, get = get, setsolve = setsolve, getsolve = setsolve)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#will check if there is a inverse exists, if not, 
+#will run makeCacheMatrix to create and store in cache the inverse
+cacheSolve <- function(matrix, ...) {
+  #check if anything in cache
+  solution <- x$getsolve()
+  #if something is available, get the result and return
+  if(!is.null(solution)) {
+    message("getting inversed matrix")
+    return(solution)
+  }
+  #no inverse in cache
+  data <- matrix$get() #get matrix
+  solution <- solve(data, ...) #create inverse
+  matirx$setsolve(solution)#save to cache
+  solution
 }
